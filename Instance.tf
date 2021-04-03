6edd2344a899ec9cf480210ed29e50f089277786
Krishnadev3699 <81144502+Krishnadev3699@users.noreply.github.com>
2021-04-03 19:06:05 +0530

    Update Instance.tf

--- Instance.tf
+++ Instance.tf
@@ -1,7 +1,7 @@
-resource "aws_instance" "TriggerByTF" {
+resource "aws_instance" "test" {
         ami = "ami-0b4ae44071f50f757"
         instance_type = "t2.micro"
  tags = {
-     Name = "Demo-Instance"
+     Name = "Demo"
         }
 }
