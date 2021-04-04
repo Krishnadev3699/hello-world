df2525005152f436956312221c637efff9d5f6b8
Krishnadev3699 <81144502+Krishnadev3699@users.noreply.github.com>
2021-04-04 15:39:24 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    access_key = "AKIA5W77SI3NZ7WCE6HU"
-    secret_key = "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"
+    access_key = "AKIA5W77SI3N57UDS6GH"
+    secret_key = "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"
     region = "ap-south-1"
 }
